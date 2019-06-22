ab8ff8b265e21ed4968d61e9f186aa6d26d23de2
Kim Jun Sik <jinaganda5@gmail.com>
2019-06-23 04:57:23 +0900

    Server Setup
    Connet Front-end with Back-end

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# RealTime_PaintingGame
+# Guess Mind 
+
+Build RealTime Drawing Game with JavaScript, Gulp and Socket.IO
+
+## To Do:
+
+-[x] Server 
+-[x] Pug
+-[] Static
+-[] SocketIO
--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
+import { join } from "path";
 import express from "express";
+import socketIO from "socket.io";
+
+const PORT = 5500;
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(express.static(join(__dirname, "static")));
+
+const handleHome = (req, res) => {
+  res.render("home");
+};
+
+app.get("/", handleHome);
+
+const handleListening = () =>
+  console.log(`✅  Server running: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
