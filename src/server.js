547bfa7aa0f7b6cd74c05bef2ea870e2893d4766
Kim Jun Sik <jinaganda5@gmail.com>
2019-06-23 11:56:33 +0900

    Check the connetion of Socket.IO

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 5500;
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
 const handleHome = (req, res) => {
@@ -20,4 +22,12 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on("connection", socket => {
+  sockets.push(socket.id);
+});
+
+setInterval(() => console.log(sockets), 3000);
