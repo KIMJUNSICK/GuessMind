5ef8a8c64dc2f25fa744eb47903f25205b66aad3
Kim Jun Sik <jinaganda5@gmail.com>
2019-06-27 19:27:35 +0900

    Complete Player Board

--- assets/js/players.js
+++ assets/js/players.js
@@ -1,3 +1,13 @@
-export const handlePlayerUpdate = ({ sockets }) => {
-  console.log(sockets);
+const board = document.getElementById("jsPBoard");
+
+const addPlayer = players => {
+  console.log(players);
+  board.innerHTML = "";
+  players.forEach(player => {
+    const playerElement = document.createElement("span");
+    playerElement.innerText = `${player.nickname} : ${player.point}`;
+    board.appendChild(playerElement);
+  });
 };
+
+export const handlePlayerUpdate = ({ sockets }) => addPlayer(sockets);
